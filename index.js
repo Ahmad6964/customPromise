fe3b930efa3458d5fe56a6908493041b090a1a1d
Ahmad6964 <109518885+Ahmad6964@users.noreply.github.com>
2022-10-25 19:16:25 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,60 @@
-class myPromise{
-    constructor(handlerFun){
-        this.state = "pending"
-        this.value = null
+class myPromise {
+  constructor(handlerFun) {
+    this.state = "pending";
+    this.value = null;
 
-        const resolve = (result) => {
-            if(this.state === "pending"){
-                this.state = "fulfilled"
-                this.value = result
-            }
-        }
+    const resolve = (result) => {
+      if (this.state === "pending") {
+        this.state = "fulfilled";
+        this.value = result;
+      }
+    };
 
-        const reject = (result)=> {
-            if(this.state === "pending"){
-                this.state = "rejected"
-                this.value = result
-            }
-        }
-            handlerFun(resolve , reject);
+    const reject = (result) => {
+      if (this.state === "pending") {
+        this.state = "rejected";
+        this.value = result;
+      }
+    };
+    handlerFun(resolve, reject);
 
-            this.then = function(succesCb){
-                if(this.state === "fulfilled"){
-                    succesCb(this.value);
-                }
-            }
+    this.then = function (succesCb) {
+      if (this.state === "fulfilled") {
+        succesCb(this.value);
+      }
+    };
 
-            this.catch = function(failureCb){
-                if(this.state === "rejected"){
-                    failureCb(this.value);
-                }
-            }
-       
-    }
+    this.catch = function (failureCb) {
+      if (this.state === "rejected") {
+        failureCb(this.value);
+      }
+    };
+  }
 }
 
 // Code Testing
-const test1 = new myPromise((resolve,reject)=>{
+
+setTimeout(() => {
+  const test1 = new myPromise((resolve, reject) => {
     resolve("resolved");
-});
-const test2 = new myPromise((resolve , reject)=>{
-    reject("rejected")
-})
-
-test1.then((res)=>{
+  });
+  test1.then((res) => {
     console.log(res);
-})
-test1.catch((err)=>{
-    console.log(err)
-})
-
-test2.then((res)=>{
-    console.log(res);
-})
-test2.catch((err)=>{
+  });
+  test1.catch((err) => {
     console.log(err);
-})
+  });
+}, 2000);
+
+setTimeout(() => {
+  const test2 = new myPromise((resolve, reject) => {
+    reject("rejected");
+  });
+
+  test2.then((res) => {
+    console.log(res);
+  });
+  test2.catch((err) => {
+    console.log(err);
+  });
+}, 4000);
